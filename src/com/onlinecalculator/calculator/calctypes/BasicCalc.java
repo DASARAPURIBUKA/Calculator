40db8082df714053a88e0ab8fec7354970ba2606
Istar <Istar@192.168.100.9>
2017-05-20 18:47:49 +0530

    up dated by D.RIBUKA

--- src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
+++ src/com/onlinecalculator/calculator/calctypes/BasicCalc.java
@@ -1,49 +1,106 @@
 package com.onlinecalculator.calculator.calctypes;
 import java.util.Scanner;
 
-public class BasicCalc
+class Calc1
 {
-	
-	public BasicCalc()
+	int a,b,c,result, ch;
+	public Calc1()
         {
-		
+		a=0;
+		b=0;
+		c=0;
+		result=0;
+		ch=0;
 	}
 	
 	public int menu()
         {
-    	
+		System.out.println("..select option..");
+		System.out.println("..addition..");
+		System.out.println("..subtraction..");
+		System.out.println("..multiplication..");
+		System.out.println("..division..");
+		System.out.println("..enter your choice..");
+		Scanner x=new Scanner(System.in);
+		ch=x.nextInt();
+		return 0;
+		
+			
 	}
-	
+
     public void userInputs()
     {
     	
+    	Scanner x=new Scanner(System.in);
     	
+    	System.out.println("enter first number");
+    	a=x.nextInt();
+    	System.out.println("enter first number");
+    	b=x.nextInt();
     }
 
     public void performOperation()
 {
-    	
+		switch (ch)
+		{
+		case 1:
+			System.out.println("..addition..");
+			result=add();
+			System.out.println("..result.."+result);
+			break;
+		case 2:
+			System.out.println("..subtracion..");
+			result=subtract();
+			System.out.println("..result.."+result);
+			break;
+		case 3:
+			System.out.println("..multiplication..");
+			result=multiply();
+			System.out.println("..result.."+result);
+			break;
+		case 4:
+			System.out.println(".division..");
+			result=divide();
+			System.out.println("..result.."+result);
+			break;
+		
+			
+		}    	
         }
        
-    }
+    
     
 	int add()
-	{
+	{	
+		c=a+b;
+		return c;
+	
 		
-	}
+}
 	
 	int subtract()
 	{
-		
+	c=a-b;
+	return c;
 	}
 	
 	int multiply()
 	{
-		
+		c=a*b;
+		return c;
 	}
 	
 	int divide()
 	{
+		c=a/b;
+		return c;
 	}
+	public static void main(String arg [])
+	{
+	Calc1 c=new Calc1();
+	c.userInputs();
+	c.menu();
 	
+	c.performOperation();
+}
 }
