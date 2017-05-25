d92050d400f55c807aedf772ae9e1f437776e920
Istar <Istar@192.168.100.6>
2017-05-25 09:18:00 +0530

    relational operators

---
+++ src/relationoptr.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class relationoptr
+
+
+{
+
+		public static void main(String args[])
+		
+		{
+
+			int a,b;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("enter first number");
+			a=sc.nextInt();
+			System.out.println("enter second number");
+			b=sc.nextInt();
+			
+			System.out.println(" a==b is  "+(a==b));
+			System.out.println("a!=b is "+(a!=b));
+					
+			System.out.println(" a>b is "+(a>b));
+			System.out.println(" a<b is  "+(a<b));
+			System.out.println("a<=b is "+(a<=b));
+					
+			System.out.println(" a>=b is "+(a>=b));
+							
+			
+		}
+		
+
+
+}
