d382545bb9cb0274159b72629e72170c746649a4
HOME <HOME@192.168.0.19>
2017-06-01 10:00:21 -0700

    Calculator Main Program Code- Ribuka

--- src/com/onlinecalculator/calculator/maincalc/MainPgm.java
+++ src/com/onlinecalculator/calculator/maincalc/MainPgm.java
@@ -6,26 +6,47 @@ import com.onlinecalculator.calculator.calctypes.*;
 
 public class MainPgm {
 public static void main(String args[]){
-        char type;
-        type=getType();
-        if(type=='a')
+	int x;
+	x=disp();
+        if(x==1)
         {
-        	
+        BasicCalc calc=new BasicCalc();
+        calc.menu();
+        calc.userInputs();
+        calc.performOperation();
         }
-        if(type=='b')
+        if(x==2)
         {
-       		
+        	ScientificCalc scalc=new ScientificCalc();
+       		scalc.menu();
+       		scalc.userInputs();
+       		scalc.performOperation();
        	}
         	
-        	if(type=='c')
+        	if(x==3)
         	{
-        		
+        	StatisticCalc scalc=new StatisticCalc();
+        	scalc.menu();
+        	scalc.userInputs();
+        	scalc.performOperation();
         	}
  
     }
         
-    static char getType()
+    static int disp()
+    
     {
+    	int ch;
+    	Scanner sc=new Scanner(System.in);
+    	System.out.println("..................CALCULATOR..........");
+    	System.out.println("................1.BASIC CALCULATOR..........");
+    	System.out.println("................2.SCIENTFIC CALCULATOR..........");
+    	System.out.println("................3.STATISTIC CALCULATOR..........");
+    	System.out.println("*************************************************");
+    	System.out.println("ENTER YOUR CHOICE");
+    	ch=sc.nextInt();
+    	return(ch);
+    	
     	
     }
 }
